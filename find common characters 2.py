5b896e839aaf06370b1f2302395cc5f6ff650e07
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-07-20 15:22:47 +0300

    Code Optimization and Review

--- find common characters 2.py
+++ find common characters 2.py
@@ -1,29 +1,23 @@
+from collections import Counter
+from typing import List
+
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        charArray = [0] * 26
+        char_array = [0] * 26
         offset = ord('a')
-        
+
         original_letters = Counter(words[0])
         for char, count in original_letters.items():
-            charArray[ord(char) - offset] = count
+            char_array[ord(char) - offset] = count
 
         for i in range(1, len(words)):
             letters = Counter(words[i])
 
-            # this block of code helps track the letters in the first word
-            for letter in list(original_letters.keys()):
-                if letter not in letters:
-                    charArray[ord(letter) - offset] = 0
-                    del original_letters[letter]
+            # Update char_array based on the characters present in each word
+            for char in list(original_letters.keys()):
+                char_array[ord(char) - offset] = min(char_array[ord(char) - offset], letters.get(char, 0))
 
-            # for letters that come starting from the second word take the minimum between the current count of occurence and the previous
-            for char, count in letters.items():
-                cur = ord(char) - offset
-                charArray[cur] = min(count, charArray[cur])
-        
-        # append all the common characters and their duplicates
-        commonChars = []
-        for ascii in range(len(charArray)):
-            commonChars.extend([chr(offset + ascii)] * (charArray[ascii]))
-        
-        return commonChars
+        # Use list comprehension to construct the result directly
+        common_chars = [chr(offset + idx) for idx, count in enumerate(char_array) for _ in range(count)]
+
+        return common_chars
